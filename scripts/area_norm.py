cc89e683350d1e23d85b113b22d9c572f022e335
adamjelly <adamturner89@gmail.com>
2018-07-26 13:47:54 +0900

    Commented the area normalisation script

--- scripts/area_norm.py
+++ scripts/area_norm.py
@@ -14,20 +14,28 @@ from scipy import integrate as sp
 spectra = pd.read_csv(argv[1], header=None)
 print(spectra)
 
+# Assign the first column to X axis values
 x = spectra.iloc[:,0]
 
+# Assign the second colum as y values
 y = spectra.iloc[:,1]
 print("This is column")
 print(y)
 
+# Integrate over all the y values using the trapezoidal rule and then normalise
+# by area
 areaT = np.trapz(y)
 y_normT = y / areaT
 
+# Integrate over all y values using Simpson integration and then normalise by
+# area
 areaS = sp.simps(y)
 y_normS = y / areaS
 
 print(areaT)
 print(areaS)
 
+# Plot the output of normalisation using both the trapezoidal rule and the
+# Simpson rule
 mp.plot(x,y_normT,x,y_normS,linewidth=2.0)
 mp.show()
